9efe78153d23a7dd3f83a6d1aeb50758f4fd3792
vanessakang223 <kangv618@newschool.edu>
2023-06-18 02:16:08 +0800

    black background

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+var defaultImageSrc = "./36days- E.gif"; // Specify the path to the default image
+
 function displayImage(src) {
   var displayedImage = document.getElementById("displayed-image");
   displayedImage.src = src;
 }
+
+// Set the default image on page load
+window.addEventListener("load", function() {
+  displayImage(defaultImageSrc);
+});
